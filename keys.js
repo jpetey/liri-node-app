be446f2d0d62d6273814223b745189cc00d517ea
jpetey <jeremee.peters@gmail.com>
2017-03-31 18:49:02 -0700

    First batch of files created; Twitter created; Twitter keys generated

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'goURKneBIWdRtPwJFHT1C1I1h',
+  consumer_secret: 'CP6CMhCyMZ8r8R0nFHjIrZm5fzdhaZVMWO2s1Lj7CRO4FwUZHM',
+  access_token_key: '127291395-2qeruoDMMPFdPrkKX8P5nDcYWAdno5aeyX4qlJQW',
+  access_token_secret: 'TdUutZ6PTDodCdJ8wyephnnwadLd9exVoERFIPCsSv1YK',
+}
