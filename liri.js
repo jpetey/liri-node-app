ecb75467dca3c4d3ef468b88d4da539245efe938
jpetey <jeremee.peters@gmail.com>
2017-04-04 14:34:54 -0700

    do-what-it-says command now functioning

--- liri.js
+++ liri.js
@@ -29,8 +29,194 @@ var userInput = process.argv[2];
 
 // BEGIN USER CHOICES
 
+
+// What happens when the user types "do-what-it-says"
+if (userInput === "do-what-it-says") {
+
+	// Read from the "random.txt" file
+	fs.readFile("random.txt", "utf8", function(error, data) {
+		// Log contents of random.txt
+  		// console.log(data);
+  		// Split data by commas (create an array) to make it more readable
+  		var dataArray = data.split(",");
+		// Log the new array
+	  	// console.log(dataArray);
+
+	  	// Set a variable equal to index[0] of array in order to pull "command" (spotify-this-song)
+	  	userCommandViaTxt = dataArray[0];
+	  	// console.log(userCommandViaTxt); 
+
+	  	// Set a variable equal to index[1] of array in order to pull song 
+	  	userCommand2ViaTxt = dataArray[1];
+	  	// console.log(userCommand2ViaTxt);
+
+	  	if (userCommandViaTxt === "my-tweets") {
+
+			// Get Tweet data from twitter based on params	
+			client.get('statuses/user_timeline.json', params, function(error, tweets, response) {
+			  // If no error occurs, log user (who's screen_name is specified in params) last 7 tweets
+			  if (!error) {
+			  	// Intro message to tweet list
+			  	console.log("\nJP's Last Tweets!\nDisclaimer: I just created my Twitter so these are all the Tweets I have")
+			  	console.log("-------------------------");	  	
+			  	// Create a loop to loop through last 7 tweets
+			  	for (var i = 0; i < 6; i++) {
+			        // Log tweet texts
+			    	console.log((i + 1) + ". " + tweets[i].text);
+			    	// Log tweet creation dates
+			    	console.log("Tweeted at: " + tweets[i].created_at + "\n\n");			
+				// If error occurs, log error message
+				} 
+			} else if (error) {
+				console.log("An error occured: " + error)
+			}
+		});
+
+		// What happens when the user types 'spotify-this-song'
+		} else if (userCommandViaTxt === "spotify-this-song") {
+			
+			// If the user doesn't type a song in...	
+			if (userCommand2ViaTxt === "") {
+				// Request info for The Sign by Ace of Basing (using song ID) from Spotify API
+				spotify.lookup({ type: 'track', id: "0hrBpAOgrt8RXigk83LLNE" }, function(err, data) {
+				    // If error occurs, log error message
+				    if ( err ) {
+				        console.log('Error occurred: ' + err);
+				        return;
+				    // If no errors occur, log data for the song...
+				    } else {
+				    	console.log("-------------------------");
+				    	// Log intro message
+				    	console.log("You didn't choose a song... Ace of Base it is!:\n");
+				    	// Log artist info
+				    	console.log("Artist: " + data.artists[0].name);
+				    	// Log track name
+				    	console.log("Song Name: " + data.name);
+				    	// Log album info of track
+				    	console.log("Album: " + data.album.name);
+				    	// Provide a link to song clip
+				    	console.log("Listen to your song here: " + data.preview_url);
+				    }
+				});
+			
+			// If the user types a song in...
+			} else {
+				
+				spotify.search({ type: 'track', query: userCommand2ViaTxt }, function(err, data) {
+				    // If error occurs, log error message
+				    if ( err ) {
+				        console.log('Error occurred: ' + err);
+				        return;
+				    // If no errors occur, log data for the song...
+				    } else {
+				    	console.log("-------------------------");
+				    	// Log intro message
+				    	console.log("Great song choice! Here are more details:\n");
+				    	// Log artist info
+				    	console.log("Artist: " + data.tracks.items[0].artists[0].name);
+				    	// Log track name
+				    	console.log("Song Name: " + data.tracks.items[0].name);
+				    	// Log album info of track
+				    	console.log("Album: " + data.tracks.items[0].album.name);
+				    	// Provide a link to song clip
+				    	console.log("Listen to your song here: " + data.tracks.items[0].preview_url);
+				    }
+				});
+			}
+
+		// What happens when the user types 'movie-this'
+		} else if (userCommandViaTxt === "movie-this") {
+
+			// If the user doesn't type a movie in...	
+			if (userCommand2ViaTxt === "") {
+				// Request info for Mr. Nobody from OMDB API
+				request('http://www.omdbapi.com?t=mr+nobody', function (error, response, body) {
+				 	// If error occurs, log error message
+				 	if (error) {
+						console.log('Error occurred: ' + error);
+						return;
+				  	// If not errors occur...
+				  	} else if (body) {
+				  		// Print the HTML for the page. 
+					  	var omdbObject = JSON.parse(body); 
+					  	// console.log(omdbObject)
+
+					  	console.log("-------------------------");
+
+					  	// Log Intro messages
+					  	console.log("You didn't choose a movie... Mr. Nobody it is!")
+					  	console.log("If you haven't watched it, you should. It's on Netflix! Here are more deets:\n")
+					  	// Log the title of the movie.
+						console.log("Movie Title: " + omdbObject.Title);
+						// Log the Year the movie came out.
+						console.log("Year Released: " + omdbObject.Year);
+						// Log the IMDB Rating of the movie.
+						console.log("IMDB Rating: " + omdbObject.imdbRating);
+						// Log the Country where the movie was produced.
+						console.log("Country Produced: " + omdbObject.Country);
+						// Log the Language of the movie.
+						console.log("Language: " + omdbObject.Language);
+						// Log the Plot of the movie.
+						console.log("Plot: " + omdbObject.Plot);
+						// Log the Actors in the movie.
+						console.log("Cast: " + omdbObject.Actors);
+						// Log the Rotten Tomatoes Rating.
+						console.log("Rotten Tomatoes Rating: " + omdbObject.Ratings[1].Value);
+						// Log the movie URL.
+						console.log("Website: " + omdbObject.Website);
+
+						console.log("-------------------------");
+					}
+				});
+			// If the user types a movie in...	
+			} else {
+				
+				// Request info for that user-input from Spotify API
+				request('http://www.omdbapi.com?t=' + userCommand2ViaTxt, function (error, response, body) {
+				 	if (error) {
+						console.log('Error occurred: ' + error);
+						return;
+				  
+				  	} else if (body) {
+				  		// Print the HTML for the page. 
+					  	var omdbObject = JSON.parse(body); 
+					  	// console.log(omdbObject)
+					  	console.log("-------------------------");
+					  	console.log("Great Movie Choice! Here are more details.\n")
+					  	//LOG DETAILS:
+							// Title of the movie.
+							console.log("Movie Title: " + omdbObject.Title);
+							// Year the movie came out.
+							console.log("Year Released: " + omdbObject.Year);
+							// IMDB Rating of the movie.
+							console.log("IMDB Rating: " + omdbObject.imdbRating);
+							// Country where the movie was produced.
+							console.log("Country Produced: " + omdbObject.Country);
+							// Language of the movie.
+							console.log("Language: " + omdbObject.Language);
+							// Plot of the movie.
+							console.log("Plot: " + omdbObject.Plot);
+							// Actors in the movie.
+							console.log("Cast: " + omdbObject.Actors);
+							// Rotten Tomatoes Rating.
+							if (omdbObject.Ratings[1] === undefined) {
+								console.log("Rotten Tomatoes Rating: None");
+							} else {
+								console.log("Rotten Tomatoes Rating: " + omdbObject.Ratings[1].Value);
+							}
+							// Rotten Tomatoes URL.
+							console.log("Website: " + omdbObject.Website);
+
+							console.log("-------------------------");
+					}
+				});
+			}
+		} 
+
+	});
+
 // What happens when the user types 'my-tweets'
-if (userInput === "my-tweets") {
+} else if (userInput === "my-tweets") {
 
 	// Get Tweet data from twitter based on params	
 	client.get('statuses/user_timeline.json', params, function(error, tweets, response) {
@@ -52,8 +238,8 @@ if (userInput === "my-tweets") {
 	}
 });
 
-// What happens when the user types 'spotify-my-song'
-} else if (userInput === "spotify-my-song") {
+// What happens when the user types 'spotify-this-song'
+} else if (userInput === "spotify-this-song") {
 	// Define a variable equal to Command Prompt user song input
 	var userSong = process.argv;
 	// Define a variable to store user input into a string
@@ -97,7 +283,7 @@ if (userInput === "my-tweets") {
 		    } else {
 		    	console.log("-------------------------");
 		    	// Log intro message
-		    	console.log("Great song choice! Here are more details?:\n");
+		    	console.log("Great song choice! Here are more details:\n");
 		    	// Log artist info
 		    	console.log("Artist: " + data.tracks.items[0].artists[0].name);
 		    	// Log track name
@@ -191,7 +377,11 @@ if (userInput === "my-tweets") {
 					// Actors in the movie.
 					console.log("Cast: " + omdbObject.Actors);
 					// Rotten Tomatoes Rating.
-					console.log("Rotten Tomatoes Rating: " + omdbObject.Ratings[1].Value);
+					if (omdbObject.Ratings[1] === undefined) {
+						console.log("Rotten Tomatoes Rating: None");
+					} else {
+						console.log("Rotten Tomatoes Rating: " + omdbObject.Ratings[1].Value);
+					}
 					// Rotten Tomatoes URL.
 					console.log("Website: " + omdbObject.Website);
 
@@ -199,51 +389,7 @@ if (userInput === "my-tweets") {
 			}
 		});
 	}
-
-// What happens when the user types "do-what-it-says"
-} else if (userInput === "do-what-it-says") {
-
-	// Read from the "random.txt" file
-	fs.readFile("random.txt", "utf8", function(error, data) {
-		// Log contents of random.txt
-  		console.log(data);
-  		// Split data by commas (create an array) to make it more readable
-  		var dataArray = data.split(",");
-		// Log the new array
-	  	// console.log(dataArray);
-
-	  	// Set a variable equal to index[0] of array in order to pull "command" (spotify-this-song)
-	  	var userInput = dataArray[0];
-	  	// console.log(userCommandFromTxt); 
-
-	  	// Set a variable equal to index[1] of array in order to pull song 
-	  	var songFromTxt = dataArray[1];
-	  	// console.log(songFromTxt);
-
-		spotify.search({ type: 'track', query: songFromTxt }, function(err, data) {
-		    // If error occurs, log error message
-		    if ( err ) {
-		        console.log('Error occurred: ' + err);
-		        return;
-		    // If no errors occur, log data for the song...
-		    } else {
-		    	console.log("-------------------------");
-		    	// Log intro message
-		    	console.log("Great song choice! Here are more details?:\n");
-		    	// Log artist info
-		    	console.log("Artist: " + data.tracks.items[0].artists[0].name);
-		    	// Log track name
-		    	console.log("Song Name: " + data.tracks.items[0].name);
-		    	// Log album info of track
-		    	console.log("Album: " + data.tracks.items[0].album.name);
-		    	// Provide a link to song clip
-		    	console.log("Listen to your song here: " + data.tracks.items[0].preview_url);
-		    }
-		});
-
-	});
-
-
-
 }
+
+
 
