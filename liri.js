c8ab50a8bdab11ea9176fcea6f297f3ddedb47b8
jpetey <jeremee.peters@gmail.com>
2017-04-03 20:57:31 -0700

    Tweets & Spotifying now active

--- liri.js
+++ liri.js
@@ -1,30 +1,62 @@
-// Incorporate the "twitter" npm package
-var Twitter = require('twitter');
-
 // create a variable that allows LIRI to take in a command from terminal
 var userInput = process.argv[2];
 
-// requiring our twitterKeys exported from keys.js
-var twitKeys = require("./keys.js");
+//TWITTER COMPONDENTS
+	// Incorporate the "twitter" npm package
+	var Twitter = require('twitter');
 
-var client = new Twitter(twitKeys.twitterKeys);
+	// requiring our twitterKeys exported from keys.js
+	var twitKeys = require("./keys.js");
 
-// Create a way to show your last 20 tweets and when they were created in your terminal/bash window.
-var params = {
-	screen_name: 'themontymonster',
-	};
+	var client = new Twitter(twitKeys.twitterKeys);
 
-client.get('statuses/user_timeline.json', params, function(error, tweets, response) {
-  if (!error) {
+	// Create a way to show your last 20 tweets and when they were created in your terminal/bash window.
+	var params = {
+		screen_name: 'themontymonster',
+		};
 
-  	console.log("\nJP's Last 20 Tweets!\n")
-  	
-  	for (var i = 0; i < 5; i++) {
-        
-    	console.log((i + 1) + ". " + tweets[i].text + "\n");
-    	console.log("Tweeted at: " + tweets[i].created_at + "\n\n");
-	
-	}
-  }
-});
+//SPOTIFY COMPONENTS
+	// Incorporate the "spotify" npm package
+	var spotify = require('spotify');
+
+
+if (userInput === "my-tweets") {
+
+	client.get('statuses/user_timeline.json', params, function(error, tweets, response) {
+	  if (!error) {
+
+	  	console.log("\nJP's Last Tweets!\nDisclaimer: I just created my Twitter so these are all the Tweets I have")
+	  	console.log("-------------------------");
+	  	
+	  	for (var i = 0; i < 6; i++) {
+	        
+	    	console.log((i + 1) + ". " + tweets[i].text);
+	    	console.log("Tweeted at: " + tweets[i].created_at + "\n\n");		
+		}	
+	  }
+	});
+
+} else if (userInput === "spotify-this-song") {
+
+	var userSong = process.argv;
+
+	var processSong = "";
+
+	processSong = userSong.slice(3).join(' ');
+
+	spotify.search({ type: 'track', query: processSong }, function(err, data) {
+	    if ( err ) {
+	        console.log('Error occurred: ' + err);
+	        return;
+	    } else {
+	    	console.log("-------------------------");
+	    	console.log("Great song choice! Want more info?:\n");
+	    	console.log("Artist: " + data.tracks.items[0].artists[0].name);
+	    	console.log("Song Name: " + data.tracks.items[0].name);
+	    	console.log("Album: " + data.tracks.items[0].album.name);
+	    	console.log("Listen to you song here: " + data.tracks.items[0].preview_url);
+	    }
+	});
+
+}
 
